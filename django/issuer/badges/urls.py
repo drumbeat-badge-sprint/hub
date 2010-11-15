8902f105d53e8f4571cb7fa68f67a096e23c3f4c
Paul Osman <paul@eval.ca>
2010-11-14 20:30:20 -0500

    Get rid of django stuff. Making separate repositories for hub code, issuer code and backpack code.

--- django/issuer/badges/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-    (r'^$', 'badges.views.index'),
-    (r'^(?P<badge_id>[\d]+)/$', 'badges.views.badge'),
-    (r'^claim/$', 'badges.views.claim'),
-    (r'^drop/$', 'badges.views.drop'),
-    (r'^(?P<username>[\w-]+)/$', 'badges.views.badges'),          
-)
--- django/issuer/dashboard/views.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-
-@login_required
-def index(request):
-    return render_to_response('dashboard/index.html', {
-    }, context_instance=RequestContext(request))
